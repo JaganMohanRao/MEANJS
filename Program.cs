a427cf0df2fc68d354a718b0c2f77f49f27816a8
JaganMohan <jagan.ganta@gmail.com>
2021-05-18 20:18:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AddQueue
+{
+    
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string[] strAddValues = new string[] { "This is String 1", "This is String 2", "This is String 3" };
+
+            Queue queueobjects = new Queue();
+            
+            for(int i=0;i<strAddValues.Length;i++)
+                queueobjects.AddUsers(strAddValues[i]);
+
+            
+            Console.ReadKey();
+        }
+    }
+
+
+}
+
+
+
+   
---
+++ Queue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace AddQueue
+{
+    public class Queue
+    {
+        private List<string> Users = new List<string>();
+        
+        public Queue()
+        {
+            
+            TimerCallback tmCallback = RemoveSpecificItem;
+            Timer timer = new Timer(RemoveSpecificItem, "Wait 1.5 Seconds", 1500, 1500);
+            
+        }
+
+        private void RemoveSpecificItem(object state)
+        {
+            Console.WriteLine(Users.Count.ToString());
+
+            if (Users.Count > 0)
+            {
+
+                Users.RemoveAt(0);
+                Console.WriteLine("Removed the User at first index ");
+
+            }
+
+        }
+          
+        public void AddUsers(string strAddUser)
+        {
+            
+            Users.Add(strAddUser);
+            Console.WriteLine("User Added:{0}", strAddUser);
+            
+        }
+
+        public void RemoveUsers(string strUser)
+        {
+            if (Users.Count > 0)
+            {
+                Users.Remove(strUser);
+                Console.WriteLine("Removed the User :  {0}", strUser);
+            }
+        }
+
+        
+    }
+}
